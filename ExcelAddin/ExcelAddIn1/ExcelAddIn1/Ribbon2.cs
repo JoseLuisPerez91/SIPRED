862f0136cb9c051b4e43cd94217290287ffb2b3f
Eduardo Perez de la Cruz <lalo_840414@hotmail.com>
2019-01-18 20:32:57 -0600

    Open Nuevo
    Se agrego el codigo para abrir la pantalla de Nuevo

--- ExcelAddin/ExcelAddIn1/ExcelAddIn1/Ribbon2.cs
+++ ExcelAddin/ExcelAddIn1/ExcelAddIn1/Ribbon2.cs
@@ -4,18 +4,15 @@ using System.Linq;
 using System.Text;
 using Microsoft.Office.Tools.Ribbon;
 
-namespace ExcelAddIn1
-{
-    public partial class Ribbon2
-    {
-        private void Ribbon2_Load(object sender, RibbonUIEventArgs e)
-        {
+namespace ExcelAddIn1 {
+    public partial class Ribbon2 {
+        private void Ribbon2_Load(object sender, RibbonUIEventArgs e) {
 
         }
 
-        private void btnNew_Click(object sender, RibbonControlEventArgs e)
-        {
-
+        private void btnNew_Click(object sender, RibbonControlEventArgs e) {
+            Nuevo _New = new Nuevo();
+            _New.Show();
         }
 
         private void button7_Click(object sender, RibbonControlEventArgs e) {
