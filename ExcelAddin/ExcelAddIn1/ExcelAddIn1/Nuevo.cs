4ee96cec6769a73dd6dc62b533a3835a0bf1ec21
Eduardo Perez de la Cruz <lalo_840414@hotmail.com>
2019-01-18 21:24:23 -0600

    Update Nuevo.cs

--- ExcelAddin/ExcelAddIn1/ExcelAddIn1/Nuevo.cs
+++ ExcelAddin/ExcelAddIn1/ExcelAddIn1/Nuevo.cs
@@ -34,11 +34,11 @@ namespace ExcelAddIn1 {
             }
             string _newTemplate = $"{Environment.CurrentDirectory}\\{((oTipoPlantilla)cmbTipo.SelectedItem).Clave}-{cmbAnio.SelectedValue.ToString()}-{DateTime.Now.ToString("ddMMyyyyHHmmss")}.xlsx";
             string _currentTemplate = $"{Environment.CurrentDirectory}\\templates\\{_Template.Nombre}";
-            Microsoft.Office.Interop.Excel.Application _current = new Microsoft.Office.Interop.Excel.Application();
-            Microsoft.Office.Interop.Excel.Workbook _workbook = _current.Workbooks.Open(_currentTemplate, Microsoft.Office.Interop.Excel.XlUpdateLinks.xlUpdateLinksNever, true, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing);
-            _workbook.SaveAs(_newTemplate, Microsoft.Office.Interop.Excel.XlFileFormat.xlExcel12, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Microsoft.Office.Interop.Excel.XlSaveAsAccessMode.xlExclusive, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing);
-            _workbook.Close(false, Type.Missing, Type.Missing);
-            _current.Quit();
+            //Microsoft.Office.Interop.Excel.Application _current = new Microsoft.Office.Interop.Excel.Application();
+            //Microsoft.Office.Interop.Excel.Workbook _workbook = _current.Workbooks.Open(_currentTemplate, Microsoft.Office.Interop.Excel.XlUpdateLinks.xlUpdateLinksNever, true, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing);
+            //_workbook.SaveAs(_newTemplate, Microsoft.Office.Interop.Excel.XlFileFormat.xlExcel12, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Microsoft.Office.Interop.Excel.XlSaveAsAccessMode.xlExclusive, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing);
+            //_workbook.Close(false, Type.Missing, Type.Missing);
+            //_current.Quit();
             Globals.ThisAddIn.Application.Visible = true;
             Globals.ThisAddIn.Application.Workbooks.Open(_newTemplate);
         }
