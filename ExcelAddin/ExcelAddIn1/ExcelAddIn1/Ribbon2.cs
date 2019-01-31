22bb47eca89fa452c5909796a58f1027ce6d86f7
Ivan Munoz <ivan.munoz@gsb.lat>
2019-01-31 11:54:27 -0600

    Revert "Integración 31-Ene-2019"
    This reverts commit 9ac9c85f170a5bd272e3273d5a7568724753af1c.

--- ExcelAddin/ExcelAddIn1/ExcelAddIn1/Ribbon2.cs
+++ ExcelAddin/ExcelAddIn1/ExcelAddIn1/Ribbon2.cs
@@ -4,29 +4,16 @@ using System.Linq;
 using System.Text;
 using Microsoft.Office.Tools.Ribbon;
 
-namespace ExcelAddIn1 {
-    public partial class Ribbon2 {
-        private void Ribbon2_Load(object sender, RibbonUIEventArgs e) {
-
-        }
-
-        private void btnNew_Click(object sender, RibbonControlEventArgs e) {
-            Nuevo _New = new Nuevo();
-            _New.Show();
-        }
-
-        private void button7_Click(object sender, RibbonControlEventArgs e) {
-            LoadTemplate _Template = new LoadTemplate();
-            _Template.Show();
-        }
-
-        private void button3_Click(object sender, RibbonControlEventArgs e)
+namespace ExcelAddIn1
+{
+    public partial class Ribbon2
+    {
+        private void Ribbon2_Load(object sender, RibbonUIEventArgs e)
         {
-            Cruce _Cruce = new Cruce();
-            _Cruce.Show();
+
         }
 
-        private void button4_Click(object sender, RibbonControlEventArgs e)
+        private void btnNew_Click(object sender, RibbonControlEventArgs e)
         {
 
         }
